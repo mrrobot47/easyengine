dfc7a438875841babf152f2dd6153de565fc3938
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-08-23 11:28:50 +0530

    Add support for managing type list
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

---
+++ php/EE/Dispatcher/TypeCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace EE\Dispatcher;
+
+use \EE\Utils;
+
+/**
+ * The root node in the command tree.
+ *
+ * @package EE
+ */
+class TypeCommand extends CompositeCommand {
+
+	public function __construct() {
+		$this->parent = false;
+
+		$this->name = 'type';
+
+		$this->shortdesc = 'Manage EasyEngine site types.';
+	}
+
+	/**
+	 * Get the human-readable long description.
+	 *
+	 * @return string
+	 */
+	public function get_longdesc() {
+		return $this->get_global_params( true );
+	}
+
+	/**
+	 * Find a subcommand registered on the root
+	 * command.
+	 *
+	 * @param array $args
+	 *
+	 * @return \EE\Dispatcher\Subcommand|false
+	 */
+	public function find_subcommand( &$args ) {
+		$command = array_shift( $args );
+
+		Utils\load_command( $command );
+
+		if ( ! isset( $this->subcommands[ $command ] ) ) {
+			return false;
+		}
+
+		return $this->subcommands[ $command ];
+	}
+
+	/**
+	 * Get all registered subcommands.
+	 *
+	 * @return array
+	 */
+	public function get_subcommands() {
+		return parent::get_subcommands();
+	}
+}
+
