6893f9cb4339a88c63c96b81606cb03ae62a87bf
Riddhesh Sanghvi <riddhesh237@gmail.com>
2021-06-14 21:28:52 +0530

    test
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- ci/test-commands.sh
+++ ci/test-commands.sh
@@ -18,6 +18,5 @@ for command in "${sub_commands[@]}"; do
 	rsync -av --delete $command/ features/ > /dev/null
 	for file in features/*.feature; do mv "$file" "${file%.feature}_${array[2]}.feature"; done
 	echo "Running tests for $command"
-	export COMPOSE_INTERACTIVE_NO_CLI=1
 	sudo -E ./vendor/bin/behat
 done
--- features/cli.feature
+++ features/cli.feature
@@ -1,5 +1,5 @@
 Feature: CLI Command
 
   Scenario: ee uninstall works properly
-    When I run 'bin/ee cli self-uninstall --yes'
+    When I run 'sudo bin/ee cli self-uninstall --yes'
     Then ee should be deleted
